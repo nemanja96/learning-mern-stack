f55e74e324b6622556eb2f4dfc88436f331163a2
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-10-14 19:42:07 +0200

    Tasks controller and routes setup

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,30 +1,32 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get tasks
 // @route   GET /api/tasks
 // @access  Private
-const getTasks = (req, res) => {
+const getTasks = asyncHandler(async (req, res) => {
     res.status(200).send("New Tasks");
-}
+})
 
 // @desc    Set task
 // @route   POST /api/tasks
 // @access  Private
-const setTask = (req, res) => {
+const setTask = asyncHandler(async (req, res) => {
     res.status(201).send("Post Task");
-}
+})
 
 // @desc    Update task
 // @route   PUT /api/tasks/:id
 // @access  Private
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async (req, res) => {
     res.status(200).send("Update Task");
-}
+})
 
 // @desc    Delete task
 // @route   DELETE /api/tasks/:id
 // @access  Private
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
     res.status(201).send("Delete Task");
-}
+})
 
 module.exports = {
     getTasks,
