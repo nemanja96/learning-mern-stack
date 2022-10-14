044075e3bbc87665fe038e08900fbf67fa6f8abe
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-10-14 20:30:20 +0200

    Create task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,17 +1,21 @@
 const asyncHandler = require('express-async-handler');
 
+const Task = require('../models/taskModel');
+
 // @desc    Get tasks
 // @route   GET /api/tasks
 // @access  Private
 const getTasks = asyncHandler(async (req, res) => {
-    res.status(200).send("New Tasks");
+    let tasks = await Task.find();
+    
+    res.status(200).json(tasks);
 })
 
 // @desc    Set task
 // @route   POST /api/tasks
 // @access  Private
 const setTask = asyncHandler(async (req, res) => {
-    res.status(201).send("Post Task");
+
 })
 
 // @desc    Update task
